bbd409b781e1ed0a9102cf0cf4da11c698e7e2f9
Alfonso Etxeberria <alfonso@Arambi.local>
2014-01-02 18:04:49 +0100

    Creado plugins

---
+++ app/Config/plugins.php
@@ -0,0 +1,18 @@
+<?php
+$config ['AppPlugins'] = array(
+  'Cofree',
+  'Acl',
+  'Comments',
+  'I18n',
+  'Upload',
+  'Website',
+  'Management',
+  'Search',
+  'Weather',
+  'Geocoder',
+  'AssetCompress',
+  'scssphp',
+  'Rating',
+  'Recaptcha',
+  'Utility',
+);
